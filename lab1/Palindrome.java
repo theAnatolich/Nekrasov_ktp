59d4222b90f40068cf53cdf5b8cfb35fb35273c7
theAnatolich <nullset1379@gmial.com>
2018-02-23 14:25:23 +0300

    lab1

--- lab1/Palindrome.java
+++ lab1/Palindrome.java
@@ -5,10 +5,8 @@ public class Palindrome
             
         for (int i = 0; i < args.length; i++)
             {
-             String s= args[i];
-             String p=reverseString(s);  
-        if(isPalindrome(s,p)) System.out.println("yes");
-        System.out.println("no");
+             String s= args[i];       
+        System.out.println(isPalindrome(s));
             }
         }
     public static String reverseString(String s) 
@@ -17,14 +15,15 @@ public class Palindrome
         for (int i = 0; i < s.length(); i++)
             {
             p+=s.charAt(s.length()-i-1);
+		
             }
-      
+      System.out.println(p);
         return p;
      }
-     public static boolean isPalindrome(String s,String p)
+     public static boolean isPalindrome(String s)
      {
-        
-        if(s.equals(p))  return false;
+        String p=reverseString(s);
+        if(!s.equals(p))  return false;
         return true;
         
      }
