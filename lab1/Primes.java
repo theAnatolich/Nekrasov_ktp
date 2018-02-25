0da2b233fe358c6e1524990e1928fe31c8d50cbe
theAnatolich <nullset1379@gmial.com>
2018-02-25 17:56:23 +0300

    25.02.

--- lab1/Primes.java
+++ lab1/Primes.java
@@ -3,21 +3,13 @@ public class Primes{
         boolean k;
         for (int i=2;i<100;i++){
             k=true;
-            for (int j=2;j<i;j++)
-                
+            for (int j=2;j<i;j++)              
              if(i%j==0){
                 k=false;
                 break;
              }
-
              if(k==true)
-             System.out.println(i);
-                
-             
-        }   
-        
-                
-    }
-    
-    
+             System.out.println(i);          
+        }                 
+    }      
 }
